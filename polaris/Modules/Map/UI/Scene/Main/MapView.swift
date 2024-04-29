b422a0e078a97499e5adc367a933c57905faa30d
Jeric Miana <jericvmiana@gmail.com>
2024-04-29 18:44:03 +0800

    Add `MapBox` package

--- polaris/Modules/Map/UI/Scene/Main/MapView.swift
+++ polaris/Modules/Map/UI/Scene/Main/MapView.swift
@@ -6,11 +6,14 @@
 //
 
 import SwiftUI
+@_spi(Experimental) import MapboxMaps
 
 struct MapView: View {
     var body: some View {
         ZStack {
-            Text("Map View")
+            let center = CLLocationCoordinate2D(latitude: 39.5, longitude: -98.0)
+            Map(initialViewport: .camera(center: center, zoom: 2, bearing: 0, pitch: 0))
+                .ignoresSafeArea()
         }
     }
 }
